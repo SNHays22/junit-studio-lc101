6c1d5ec8905c9919d5b81facc307c35608911884
SNHays22 <codergrl22@outlook.com>
2022-09-12 20:28:04 -0500

    studio class 5 testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,59 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test //test 1
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test //test 2
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test //test 3
+    public void onlySingleBracketsWithContent(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test //test 4
+    public void onlySingleBracketsInterlaced(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test //test 5
+    public void onlySingleBracketsBeforeContent(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test //test 6
+    public void twoSetsBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode[]"));
+    }
+
+    @Test //test 7
+    public void nestedBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
+    }
+
+    @Test //test 8
+    public void onlyRightBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test //test 9
+    public void onlyLeftBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test //test 10
+    public void outOfOrderBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launch[code"));
+    }
+
+    @Test //test 11
+    public void mismatchReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][][[[][]"));
+    }
 
 }
